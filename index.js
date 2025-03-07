967f18d5a59a62f6a281a85133bfcb3a6303fae3
ssk090 <shivanandasai.38@gmail.com>
2025-03-07 21:50:43 +0530

    Hasing added

--- index.js
+++ index.js
@@ -2,26 +2,48 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const { UserModel, TodoModel } = require("./db");
 const { JWT_SECRET, auth } = require("./auth");
-
+const bcrypt = require('bcrypt')
+const { z } = require('zod');
 
 const app = express();
 app.use(express.json());
 
-
 app.post("/signup", async (req, res) => {
+    const requiredBody = z.object({
+        email: z.string().email(),
+        name: z.string().min(3).max(16),
+        password: z.string().min(3).max(10)
+    })
+
+    const parsedData = requiredBody.safeParse(req.body)
+
+    if (!parsedData.success) {
+        res.status(403).json({
+            message: "Incorrect format",
+            error: parsedData.error
+        })
+        return
+    }
+
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
 
-    await UserModel.create({
-        name,
-        email,
-        password,
-    })
-    res.json({
-        message: "Signed up successful"
-    })
-
+    const hashedPassword = await bcrypt.hash(password, 5);
+    try {
+        await UserModel.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        res.status(200).json({
+            message: "Signed up successful"
+        });
+    } catch (error) {
+        res.status(403).json({
+            message: "duplicated email"
+        });
+    }
 })
 
 app.post("/login", async (req, res) => {
@@ -30,10 +52,11 @@ app.post("/login", async (req, res) => {
 
     const user = await UserModel.findOne({
         email,
-        password
     })
 
-    if (user) {
+    const passwordMatched = await bcrypt.compare(password, user.password)
+
+    if (passwordMatched) {
         const token = jwt.sign({
             user: user._id.toString()
         }, JWT_SECRET)
